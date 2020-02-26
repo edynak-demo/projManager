c99b608da93d2bfaff65dc28f707ad30007db414
edynak-demo <edynakdemo@gmail.com>
2020-02-26 10:46:05 -0500

    added create and delete tasks feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,19 @@
 class TasksController < ApplicationController
-	
+		before_action :set_project
 
 	def create
-		
+		@task = @project.tasks.create(task_params)
+		redirect_to @project
 	end
 
 	def destroy
-		
+		@task = @project.tasks.find(params[:id])
+		if @task.destroy
+			flash[:success] = "Task was deleted."
+		else
+			flash[:error] = "Task was not deleted."
+		end
+		redirect_to @project
 	end
 
 	def complete
