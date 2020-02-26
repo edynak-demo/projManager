7ded3d545ae121e4cf3989ce11c1b8b7d28a4fbc
edynak-demo <edynakdemo@gmail.com>
2020-02-26 11:00:47 -0500

    built out the complete tasks feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
 		before_action :set_project
+		before_action :set_task, except: [:create]
 
 	def create
 		@task = @project.tasks.create(task_params)
@@ -17,7 +18,8 @@ class TasksController < ApplicationController
 	end
 
 	def complete
-		
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project, notice: "Task Completed"
 	end
 
 	private
